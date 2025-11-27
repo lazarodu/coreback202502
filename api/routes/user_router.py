1351125927a0cdbc619df9063b46235014b3ce3c
lazarodu <lazarodu@gmail.com>
2025-11-27 18:35:18 -0300

    feat: user route

--- api/routes/user_router.py
+++ api/routes/user_router.py
@@ -1,6 +1,11 @@
 from fastapi import APIRouter
+from api.schemas.user_schemas import UserCreate
+from core.factories.use_case_factory import UseCaseFactory
 
 router = APIRouter()
 
 @router.post("/users")
-async def create_user()
+async def create_user(
+    user: UserCreate,
+    factory: UseCaseFactory
+):
